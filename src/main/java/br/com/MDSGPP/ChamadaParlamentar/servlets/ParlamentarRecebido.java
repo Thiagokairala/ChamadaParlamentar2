0c7870e41c539f37916e30752d495085bf816d42
Thiago Ramires Kairala <thiagor.kairala@gmail.com>
2013-10-31 21:39:39 -0200

    tirando servlets da control e criando um pacote especifico para os mesmos

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/ParlamentarRecebido.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/ParlamentarRecebido.java
@@ -1,4 +1,4 @@
-package br.com.MDSGPP.ChamadaParlamentar.control;
+package br.com.MDSGPP.ChamadaParlamentar.servlets;
 
 import java.io.IOException;
 
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.MDSGPP.ChamadaParlamentar.control.EstatisticaControl;
 import br.com.MDSGPP.ChamadaParlamentar.model.Estatistica;
 
 
