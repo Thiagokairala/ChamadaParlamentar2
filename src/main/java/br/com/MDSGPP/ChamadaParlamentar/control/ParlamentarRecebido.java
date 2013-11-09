b8d7e92a3132c98c1e41d387a29b0d3e14529b94
Thiago Ramires Kairala <thiagor.kairala@gmail.com>
2013-11-09 18:55:33 -0200

    servlet conectando normalmente

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/control/ParlamentarRecebido.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/control/ParlamentarRecebido.java
@@ -1,9 +1,5 @@
 package br.com.MDSGPP.ChamadaParlamentar.control;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
