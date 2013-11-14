63dec42a7466d096bcfe8198b278f84c3cf2a666
macarrao0 <brasil.eduardo1@gmail.com>
2013-11-14 13:03:41 -0200

    servlet funcionando normalmente

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/SessoesEReunioesServlet.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/servlets/SessoesEReunioesServlet.java
@@ -1,9 +1,11 @@
 package br.com.MDSGPP.ChamadaParlamentar.servlets;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,15 +15,27 @@ import br.com.MDSGPP.ChamadaParlamentar.model.Dia;
 
 public class SessoesEReunioesServlet extends HttpServlet {
 	
+	private static final long serialVersionUID = 1L;
+
 	protected void service (HttpServletRequest request, HttpServletResponse response) {
 		RequestDispatcher rd = null;
 		
 		try {
 			ArrayList<Dia> dia = DiaControl.getDias();
-			request.setAttribute("dias", dias);
+			request.setAttribute("dias", dia);
+			rd = request.getRequestDispatcher("AcompanharSessao.jsp");
 		} catch (ClassNotFoundException e) {
-			
+			rd = request.getRequestDispatcher("Erro.jsp");
 		} catch (SQLException e) {
+			rd = request.getRequestDispatcher("Erro.jsp");
+		}
+		
+		try {
+			rd.forward(request, response);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
