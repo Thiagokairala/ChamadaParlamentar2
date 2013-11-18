cce1b5a1478bdb1da4dd0d6f6d6d17b0516fd706
macarrao0 <brasil.eduardo1@gmail.com>
2013-11-18 16:51:10 -0200

    sessoesEReunioesControl testada

--- src/test/java/br/com/MDSGPP/ChamadaParlamentar/control/teste/TesteSessoesEReunioesControl.java
+++ src/test/java/br/com/MDSGPP/ChamadaParlamentar/control/teste/TesteSessoesEReunioesControl.java
@@ -2,23 +2,36 @@ package br.com.MDSGPP.ChamadaParlamentar.control.teste;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import br.com.MDSGPP.ChamadaParlamentar.control.SessoesEReunioesControl;
+import br.com.MDSGPP.ChamadaParlamentar.model.SessoesEReunioes;
 
 public class TesteSessoesEReunioesControl {
-
-	SessoesEReunioesControl sessoesEReunioes;
+	SessoesEReunioesControl sessoes;
 	
 	@Before
 	public void setUp() {
-		sessoesEReunioes = new SessoesEReunioesControl();
+		sessoes = new SessoesEReunioesControl();
 	}
-	
+
 	@Test
 	public void testSessoesEReunioesControl() {
-		assertNotNull(sessoesEReunioes);
+		assertNotNull(sessoes);
+	}
+
+	@Test
+	public void testPassarSessao() throws ClassNotFoundException, SQLException {
+		SessoesEReunioes teste = SessoesEReunioesControl.passarSessao("teste");
+		assertNotNull(teste.getDeputadosPresentes());
+		assertTrue(teste.getDeputadosPresentes().size() == 0);
+		
+		SessoesEReunioes teste2 = SessoesEReunioesControl.passarSessao("ORDINÁRIA Nº 313 - 20/11/2012");
+		assertNotNull(teste2.getDeputadosPresentes());
+		assertTrue(teste2.getDeputadosPresentes().size() > 0);		
 	}
 
 }
