d9cfe5b5085608b9f7a34d803b3f1860e7334f33
brunogagos <bbraganca1991@gmail.com>
2013-11-27 22:21:27 -0200

    alguns testes

--- src/test/java/br/com/MDSGPP/ChamadaParlamentar/control/teste/TestePartidoControl.java
+++ src/test/java/br/com/MDSGPP/ChamadaParlamentar/control/teste/TestePartidoControl.java
@@ -1,12 +1,17 @@
 package br.com.MDSGPP.ChamadaParlamentar.control.teste;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
 import br.com.MDSGPP.ChamadaParlamentar.control.PartidoControl;
+import br.com.MDSGPP.ChamadaParlamentar.dao.DeputadoDao;
+import br.com.MDSGPP.ChamadaParlamentar.model.Deputados;
+import br.com.MDSGPP.ChamadaParlamentar.model.Partidos;
 
 public class TestePartidoControl {
 
@@ -15,5 +20,13 @@ public class TestePartidoControl {
 			throws ClassNotFoundException, SQLException {
 		assertNotNull(PartidoControl.passarListaPartidos());
 	}
-
+	
+	@Test
+	public void testVerificaExistenciaRetornaNulo() throws ClassNotFoundException, SQLException{
+		PartidoControl control = new PartidoControl();
+		assertTrue(control.verificaExistencia("ronaldo") == null);
+	}
 }
+
+
+	
