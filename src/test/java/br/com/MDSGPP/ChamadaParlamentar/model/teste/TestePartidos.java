fd73bc03878fe5225592427f07b2cecebdc352bd
RafaelFazzolino <rafael_fazzolino@hotmail.com>
2013-11-28 01:09:30 -0200

    partido 100%

--- src/test/java/br/com/MDSGPP/ChamadaParlamentar/model/teste/TestePartidos.java
+++ src/test/java/br/com/MDSGPP/ChamadaParlamentar/model/teste/TestePartidos.java
@@ -1,6 +1,6 @@
 package br.com.MDSGPP.ChamadaParlamentar.model.teste;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.MDSGPP.ChamadaParlamentar.model.Deputados;
+import br.com.MDSGPP.ChamadaParlamentar.model.Estatistica;
 import br.com.MDSGPP.ChamadaParlamentar.model.Partidos;
 
 public class TestePartidos {
@@ -21,6 +22,11 @@ public class TestePartidos {
 		partido.setNomePartido("teste");
 		partido.setSigla("df");
 		partido.setDeputadosDoPartido(lista);
+		
+		ArrayList<Estatistica> array = new ArrayList<Estatistica>();
+		partido.setEstatisticaDosDeputados(array);
+		partido.setDeputadosSemDados(array);
+		
 		
 	}
 	@Test
@@ -59,4 +65,18 @@ public class TestePartidos {
 		 assertTrue(partido.getDeputadosDoPartido().size() == 1);
 	}
 
+	@Test
+	public void testSetEstatisticaDosDeputados() {
+		Estatistica estatistica = new Estatistica();
+		partido.getEstatisticaDosDeputados().add(estatistica);
+		assertTrue(partido.getEstatisticaDosDeputados().size() == 1);
+	}
+
+	@Test
+	public void testsetDeputadosSemDados() {
+		Estatistica estatistica = new Estatistica();
+		partido.getDeputadosSemDados().add(estatistica);
+		 assertTrue(partido.getDeputadosSemDados().size() == 1);
+	}
+	
 }
