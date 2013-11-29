1164976a3987c1e42771f85f0feae3daa64f5909
Thiagokairala <thiagor.kairala@gmail.com>
2013-11-29 03:27:12 -0200

    100%

---
+++ src/test/java/br/com/MDSGPP/ChamadaParlamentar/util/teste/TesteVerificarData.java
@@ -0,0 +1,20 @@
+package br.com.MDSGPP.ChamadaParlamentar.util.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.MDSGPP.ChamadaParlamentar.util.VerificarData;
+
+public class TesteVerificarData {
+
+	@Test
+	public void testVerificaData() {
+		boolean resposta = VerificarData.verificaData("12/12/2012");
+		boolean resposta2 = VerificarData.verificaData("nao deve passar");
+		
+		assertTrue(resposta);
+		assertTrue(resposta2 == false);
+	}
+
+}
