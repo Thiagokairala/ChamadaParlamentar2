6f8a3056a136e926349c872b957038bac7c35058
Thiagokairala <thiagor.kairala@gmail.com>
2013-12-09 13:04:08 -0200

    começando a alterar o ranking

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/Dao.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/dao/Dao.java
@@ -10,15 +10,18 @@ public class Dao extends ConnectionFactory {
 	}
 
 	public static void truncateTables() throws SQLException, ClassNotFoundException{
-		Dao dao = new Dao();
 		
 		String sql1 = "truncate table datas";
+		String sql2 = "truncate table ranking";
 		String sql3 = "truncate table sessao";
 		
+		
 		PreparedStatement stmt1 = ConnectionFactory.getConexao().prepareStatement(sql1);
+		PreparedStatement stmt2 = ConnectionFactory.getConexao().prepareStatement(sql2);
 		PreparedStatement stmt3 = ConnectionFactory.getConexao().prepareStatement(sql3);
 	
 		stmt1.execute();
+		stmt2.execute();
 		stmt3.execute();
 	}
 }
