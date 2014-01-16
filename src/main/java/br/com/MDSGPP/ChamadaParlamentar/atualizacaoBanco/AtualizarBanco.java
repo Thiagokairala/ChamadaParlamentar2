e6c5fe08a9aae5ffa887d5ae027b08650b410bda
Thiagokairala <thiagor.kairala@gmail.com>
2014-01-15 23:37:25 -0200

    corrigindo para servidor oficial

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/atualizacaoBanco/AtualizarBanco.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/atualizacaoBanco/AtualizarBanco.java
@@ -34,9 +34,9 @@ public class AtualizarBanco {
 }
 	
 	private final static long delayDiario = 1000*60*60*24;
-	private final static int fONE_DAY = 0;
-	private final static int fFOUR_AM = 13;
-	private final static int fZERO_MINUTES = 18;
+	private final static int fONE_DAY = 1;
+	private final static int fFOUR_AM = 6;
+	private final static int fZERO_MINUTES = 0;
 
 	private static Date getAmanha6Am(){
 	  Calendar tomorrow = new GregorianCalendar();
